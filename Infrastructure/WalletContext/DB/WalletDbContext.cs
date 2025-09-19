6b629c798577a3af89924e77f3d63b886672fc72
zohreh <zohreh.salamzadeh@gmail.com>
2025-09-19 22:32:54 +0330

    add changes

---
+++ Infrastructure/WalletContext/DB/WalletDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WalletContext.DB
+{
+    public class WalletDbContext : DbContext
+    {
+        public WalletDbContext(DbContextOptions<WalletDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Wallet> Wallet { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
+
+//dotnet ef migrations add InitialCreate
+//dotnet ef database update
--- UI/Api/Program.cs
+++ UI/Api/Program.cs
@@ -1,8 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using WalletContext.DB;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<WalletDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("WalletDb")));
 
 var app = builder.Build();
 
